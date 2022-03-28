0571d34f9e53933cf7d078aed6acf21b23b8d1bf
ma9da <dodymasry25@gmail.com>
2022-03-28 16:01:55 +0200

    added: contact component to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/about/About";
 import Baking from "./components/baking/Baking";
+import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Location from "./components/location/Location";
 import Navbar from "./components/navbar/Navbar";
@@ -12,6 +13,7 @@ function App() {
   <About/>
   <Location/>
   <Baking/>
+  <Contact/>
   </>
   );
 }
