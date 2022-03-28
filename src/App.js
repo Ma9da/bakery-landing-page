ed3f11e94b2b7f171451fc86795f465c7b057027
ma9da <dodymasry25@gmail.com>
2022-03-28 16:06:56 +0200

    added: footer component to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/about/About";
 import Baking from "./components/baking/Baking";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
 import Location from "./components/location/Location";
 import Navbar from "./components/navbar/Navbar";
@@ -14,6 +15,7 @@ function App() {
   <Location/>
   <Baking/>
   <Contact/>
+  <Footer/>
   </>
   );
 }
